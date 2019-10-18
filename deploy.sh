492b66496096ea0c3cf46abb74be6222cdeb9983
FredaFei <964959033@qq.com>
2019-10-18 12:09:57 +0800

    dll build

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:FredaFei/react-ts-env.git dll:gh-pages
+git push -f git@github.com:FredaFei/react-ts-env.git master:gh-pages
 
 cd -
